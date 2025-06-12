316bada529c5a4088b27478e3b30baeabdac67e7
Yangyang Shi <shiyang1983@gmail.com>
2025-06-12 16:44:23 -0700

    add mobile LLM

--- prismatic/models/backbones/llm/__init__.py
+++ prismatic/models/backbones/llm/__init__.py
@@ -1,4 +1,5 @@
 from .base_llm import LLMBackbone
 from .llama2 import LLaMa2LLMBackbone
 from .mistral import MistralLLMBackbone
+from .mobile_llm import MBLLMBackbone
 from .phi import PhiLLMBackbone
