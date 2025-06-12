dddb0f6eab6cbc1dffad68057888d517f5f4404a
Yangyang Shi <shiyang1983@gmail.com>
2025-06-12 14:07:46 -0700

    add mobileLLM

---
+++ prismatic/models/backbones/llm/mobileLLM.py
@@ -0,0 +1,84 @@
+"""
+llama2.py
+
+Class definition for all LLMs derived from LlamaForCausalLM.
+"""
+
+from typing import Optional, Type
+
+import torch
+
+from prismatic.models.backbones.llm.base_llm import HFCausalLLMBackbone
+from prismatic.models.backbones.llm.prompting import (
+    LLaMa2ChatPromptBuilder,
+    PromptBuilder,
+    PurePromptBuilder,
+    VicunaV15ChatPromptBuilder,
+)
+from torch import nn as nn
+from transformers import AutoModelForCausalLM
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer
+
+# Registry =>> Support LLaMa-2 Models (from HF Transformers)
+# fmt: off
+MBLLM_MODELS = {
+    # === Pure Meta LLaMa-2 (non-instruct/chat-tuned) Models ===
+    "MobileLLM-1B": {
+        "llm_family": "mbllm-1b", "llm_cls": AutoModelForCausalLM, "hf_hub_path": "facebook/MobileLLM-1B"
+    },
+}
+# fmt: on
+
+
+class MBLLMBackbone(HFCausalLLMBackbone):
+    def __init__(
+        self,
+        llm_backbone_id: str,
+        llm_max_length: int = 2048,
+        hf_token: Optional[str] = None,
+        inference_mode: bool = False,
+        use_flash_attention_2: bool = True,
+    ) -> None:
+        super().__init__(
+            llm_backbone_id,
+            llm_max_length=llm_max_length,
+            hf_token=hf_token,
+            inference_mode=inference_mode,
+            use_flash_attention_2=use_flash_attention_2,
+            **MBLLM_MODELS[llm_backbone_id],
+        )
+
+        # [Special Case] LLaMa-2 PAD Token Handling --> for clarity, we add an extra token (and resize)
+        self.tokenizer.add_special_tokens(
+            {
+                "eos_token": "</s>",
+                "bos_token": "<s>",
+                "unk_token": "<unk>",
+            }
+        )
+        self.llm.config.pad_token_id = self.tokenizer.pad_token_id
+        self.llm.resize_token_embeddings(len(self.tokenizer), pad_to_multiple_of=64)
+
+    @property
+    def prompt_builder_fn(self) -> Type[PromptBuilder]:
+        if self.identifier.startswith("mbllm-") and self.identifier.endswith("-pure"):
+            return PurePromptBuilder
+
+        elif self.identifier.startswith("mbllm-") and self.identifier.endswith("-chat"):
+            return LLaMa2ChatPromptBuilder
+
+        elif self.identifier.startswith("vicuna"):
+            return VicunaV15ChatPromptBuilder
+
+        raise ValueError(
+            f"No PromptBuilder defined for LLM Backbone `{self.identifier}`"
+        )
+
+    @property
+    def transformer_layer_cls(self) -> Type[nn.Module]:
+        return LlamaDecoderLayer
+
+    @property
+    def half_precision_dtype(self) -> torch.dtype:
+        """MBLLM was trained in BF16; see https://huggingface.co/docs/transformers/main/model_doc/llama2."""
+        return torch.bfloat16
